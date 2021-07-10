87e12eed099f291f1daaaa7174ff8e0e13ab2e3c
Cinnamon_roll <845633415@qq.com>
2021-07-10 23:41:50 +0900

    车厢方向调整

--- Assets/rrg/Script/TrainCarriage.cs
+++ Assets/rrg/Script/TrainCarriage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TrainCarriage : MonoBehaviour
 {
     public GameObject forwardPoint;
+    public GameObject railwayPoint;
     public Vector3 direction;
 
     private bool moveAround;
@@ -12,25 +13,25 @@ public class TrainCarriage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    moveAround = false;
-        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveAround = false;
+        }
 
-        //if (moveAround)
-        //{
-        //    direction = forwardPoint.transform.position - transform.position;
-        //    if (Vector3.Angle(direction, transform.right) > 90f) // 目标点在右边
-        //    {
-        //        transform.Rotate(transform.forward, Vector3.Angle(direction, transform.up) - 110);
-        //    }
-        //    else // 目标点在左边
-        //    {
-        //        transform.Rotate(transform.forward, -Vector3.Angle(direction, transform.up) + 110);
-        //    }
-        //}
-        //else
-        //{
+        if (moveAround)
+        {
+            direction = railwayPoint.GetComponentInParent<Transform>().transform.position - transform.position;
+            if (Vector3.Angle(direction, transform.right) > 90f) // 目标点在右边
+            {
+                transform.Rotate(transform.forward, Vector3.Angle(direction, transform.up) - 180 );
+            }
+            else // 目标点在左边
+            {
+                transform.Rotate(transform.forward, -Vector3.Angle(direction, transform.up) - 180);
+            }
+        }
+        else
+        {
             direction = forwardPoint.transform.position - transform.position;
             if (Vector3.Angle(direction, transform.right) > 90f) // 目标点在右边
             {
@@ -40,6 +41,14 @@ public class TrainCarriage : MonoBehaviour
             {
                 transform.Rotate(transform.forward, -Vector3.Angle(direction, transform.up) + 90);
             }
-    //    }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Railway"))
+        {
+            moveAround = true;
+            railwayPoint = collision.gameObject;
+        }
     }
 }
